a640f4e00170e2ccba777b1f628fd54b2eff84ec
Dario Castiglione <dario.castiglione99@gmail.com>
2021-11-23 16:41:43 +0100

    solve bug

--- js/index.js
+++ js/index.js
@@ -2,48 +2,62 @@ const image = document.querySelector("img")
 const inputRange = document.querySelectorAll("li input")
 const change = document.querySelector("#change-img")
 
-
-
-
-
-
-
-
+const addMod = () =>{
+    filter=`blur(${blur}px) 
+    brightness(${brightness}%)
+    contrast(${contrast}%)
+    grayscale(${grayscale}%)
+    hue-rotate(${hue_rotate}deg)
+    invert(${invert}%)
+    saturate(${saturate}%)
+    sepia(${sepia}%)`
+}
+function changing(){
+    image.style.filter = `${filter}`   
+    addMod()
+}
+let filter = ""
+let blur = 0;
 inputRange[0].addEventListener("input", (event)=>{
-    image.style.filter =`blur(${event.target.value}px)`
-    
+    blur = event.target.value 
+    changing()
 })
+let brightness = 100;
 inputRange[1].addEventListener("input", (event)=>{
-    image.style.filter =`brightness(${event.target.value}%)`
-    
+    brightness = event.target.value 
+    changing()    
 })
+let contrast = 100;
 inputRange[2].addEventListener("input", (event)=>{
-    image.style.filter =`contrast(${event.target.value}%)`
-    
+    contrast = event.target.value 
+    changing()    
 })
+let grayscale = 0;
 inputRange[3].addEventListener("input", (event)=>{
-    image.style.filter =`grayscale(${event.target.value}%)`
-    
+    grayscale = event.target.value 
+    changing()    
 })
+let hue_rotate = 0;
 inputRange[4].addEventListener("input", (event)=>{
-    image.style.filter =`hue-rotate(${event.target.value}deg)`
-    
+    hue_rotate = event.target.value 
+    changing()    
 })
+let invert = 0;
 inputRange[5].addEventListener("input", (event)=>{
-    image.style.filter =`invert(${event.target.value}%)`
-    
+    invert = event.target.value 
+    changing()    
 })
+let saturate = 100;
 inputRange[6].addEventListener("input", (event)=>{
-    image.style.filter =`saturate(${event.target.value}%)`
-    
+    saturate = event.target.value 
+    changing()   
 })
+let sepia = 0;
 inputRange[7].addEventListener("input", (event)=>{
-    image.style.filter =`sepia(${event.target.value}%)`
-    
+    sepia = event.target.value 
+    changing()  
 })
-
 change.addEventListener("click", ()=>{
     const newImg = prompt("inserisci url");
     image.src = newImg
 })
-
