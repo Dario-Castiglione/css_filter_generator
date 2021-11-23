d0b3dbb98d19bbf1bd7c30da5cb572274425ee8a
Dario Castiglione <dario.castiglione99@gmail.com>
2021-11-23 17:07:36 +0100

    .

--- js/index.js
+++ js/index.js
@@ -1,6 +1,31 @@
 const image = document.querySelector("img")
 const inputRange = document.querySelectorAll("li input")
 const change = document.querySelector("#change-img")
+const returnDefault = document.querySelector("#return-default")
+
+
+
+let blur = 0;
+let brightness = 100;
+let contrast = 100;
+let grayscale = 0;
+let hue_rotate = 0;
+let saturate = 100;
+let sepia = 0;
+let invert = 0;
+function tournDefault(){
+    blur = 0;
+    brightness = 100;
+    contrast = 100;
+    grayscale = 0;
+    hue_rotate = 0;
+    saturate = 100;
+    sepia = 0;
+    invert = 0;
+    changing()
+}
+
+returnDefault.addEventListener("click",tournDefault)
 
 const addMod = () =>{
     filter=`blur(${blur}px) 
@@ -17,42 +42,42 @@ function changing(){
     addMod()
 }
 let filter = ""
-let blur = 0;
+
 inputRange[0].addEventListener("input", (event)=>{
     blur = event.target.value 
     changing()
 })
-let brightness = 100;
+
 inputRange[1].addEventListener("input", (event)=>{
     brightness = event.target.value 
     changing()    
 })
-let contrast = 100;
+
 inputRange[2].addEventListener("input", (event)=>{
     contrast = event.target.value 
     changing()    
 })
-let grayscale = 0;
+
 inputRange[3].addEventListener("input", (event)=>{
     grayscale = event.target.value 
     changing()    
 })
-let hue_rotate = 0;
+
 inputRange[4].addEventListener("input", (event)=>{
     hue_rotate = event.target.value 
     changing()    
 })
-let invert = 0;
+
 inputRange[5].addEventListener("input", (event)=>{
     invert = event.target.value 
     changing()    
 })
-let saturate = 100;
+
 inputRange[6].addEventListener("input", (event)=>{
     saturate = event.target.value 
     changing()   
 })
-let sepia = 0;
+
 inputRange[7].addEventListener("input", (event)=>{
     sepia = event.target.value 
     changing()  
@@ -61,3 +86,4 @@ change.addEventListener("click", ()=>{
     const newImg = prompt("inserisci url");
     image.src = newImg
 })
+tournDefault()
