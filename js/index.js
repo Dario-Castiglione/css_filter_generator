5f2600ffeee7c3a9c5b456df25c109576ef27a9f
Dario Castiglione <dario.castiglione99@gmail.com>
2021-11-23 16:04:46 +0100

    feat: js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,49 @@
 const image = document.querySelector("img")
-const inputRange = document.querySelector("li input")
+const inputRange = document.querySelectorAll("li input")
+const change = document.querySelector("#change-img")
 
+
+
+
+
+
+
+
+inputRange[0].addEventListener("input", (event)=>{
+    image.style.filter =`blur(${event.target.value}px)`
+    
+})
+inputRange[1].addEventListener("input", (event)=>{
+    image.style.filter =`brightness(${event.target.value}%)`
+    
+})
+inputRange[2].addEventListener("input", (event)=>{
+    image.style.filter =`contrast(${event.target.value}%)`
+    
+})
+inputRange[3].addEventListener("input", (event)=>{
+    image.style.filter =`grayscale(${event.target.value}%)`
+    
+})
+inputRange[4].addEventListener("input", (event)=>{
+    image.style.filter =`hue-rotate(${event.target.value}deg)`
+    
+})
+inputRange[5].addEventListener("input", (event)=>{
+    image.style.filter =`invert(${event.target.value}%)`
+    
+})
+inputRange[6].addEventListener("input", (event)=>{
+    image.style.filter =`saturate(${event.target.value}%)`
+    
+})
+inputRange[7].addEventListener("input", (event)=>{
+    image.style.filter =`sepia(${event.target.value}%)`
+    
+})
+
+change.addEventListener("click", ()=>{
+    const newImg = prompt("inserisci url");
+    image.src = newImg
+})
 
